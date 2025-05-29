ce94a4f5355298da9fa3f62cc733f9d9147558bf
Shadrach Roundy <roundys@wwu.edu>
2025-05-28 18:54:01 -0700

    Added level node, which is a Canvas Layer, and implemented main to use it and ship. The game now shows the ship in front of the level background.

--- main.gd
+++ main.gd
@@ -1,5 +1,15 @@
 extends Node2D
 
+@export var Ship : PackedScene
+@export var Level : PackedScene
 
 func _ready():
-	randomize()
+	Ship = load("res://Ship.tscn")
+	Level = load("res://Level.tscn")
+	start_game()
+
+func start_game():
+	var level = Level.instantiate()
+	add_child(level)
+	var ship = Ship.instantiate()
+	add_child(ship)
