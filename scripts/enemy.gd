519d985352a82f85c04be0238f1e40f41a4f2ffa
Shadrach Roundy <roundys@wwu.edu>
2025-06-18 18:39:39 -0700

    Implemented enemy scene and got something working. Will need to figure out positioning and how to get it to shoot the right way for proper testing.

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,4 +1,29 @@
-extends Area2D
+extends RigidBody2D
+
 @export var health: int = 2
 @export var speed = 250
 @export var Bullet : PackedScene
+
+func _ready():
+	Bullet = load("res://scenes/bullet.tscn")
+	$AnimatedSprite2D.play("Idle")
+	
+func _process(_delta):
+	if $Timer.is_stopped():
+		shoot()
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
+	
+func _on_hit():
+	health -= 1
+	if health == 0:
+		$AnimatedSprite2D.play("Boom")
+		await get_tree().create_timer(0.8).timeout
+		hide()
+		
+func shoot():
+	var b = Bullet.instantiate()
+	get_tree().root.add_child(b)
+	b.global_transform = $Chamber.global_transform
+	$Timer.start()
--- scripts/level_1.gd
+++ scripts/level_1.gd
@@ -1,6 +1,12 @@
 extends ParallaxLayer
 
+@export var Enemy : PackedScene
 var scrolling_speed = 1000
 
+func _ready():
+	Enemy = load("res://scenes/enemies/test_enemy.tscn")
+	var enemy = Enemy.instantiate()
+	add_child(enemy)
+	
 func _process(delta):
 	motion_offset.x -= scrolling_speed * delta * 0.1
